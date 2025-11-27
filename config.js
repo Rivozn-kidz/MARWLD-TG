3f2fd61e5b648b9771882d37dbd0eb5d6f7ec80c
Rivozn kidz <smtechofcmodz@outlook.com>
2025-11-27 22:38:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ BOT_NAME: "MAWRLD MD",
  PREFIX: ".","/",
 OWNER_NAME: "Ridz Coder",
 OWNER_NUMBER: "263714732501",
-BOT_TOKEN: "8566420331:AAHxdrfy7SEK1Wm0ff6x4i-lShYjj0nPjVA",
+BOT_TOKEN: "",
 };
 export default config;
