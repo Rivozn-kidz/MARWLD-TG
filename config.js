1d4d776925d6b7dee183e66651afa342eba84339
Rivozn kidz <smtechofcmodz@outlook.com>
2025-11-27 23:24:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 const config = {
-BOT_NAME: "MAWRLD MD",
- PREFIX: ".",
-OWNER_NAME: "Ridz Coder",
-OWNER_NUMBER: "263714732501",
-BOT_TOKEN: "",
+  BOT_NAME: "MAWRLD MD",
+  PREFIX: ".",
+  OWNER_NAME: "Ridz Coder",
+  OWNER_NUMBER: "263714732501",
+  BOT_TOKEN: "",
+  OWNER: ["Ridz Coder", "263714732501"] 
 };
+
 export default config;
