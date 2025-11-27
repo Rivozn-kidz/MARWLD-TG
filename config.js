725e644260b26537cc17e489d8a1cc163ea4b3c1
Rivozn kidz <smtechofcmodz@outlook.com>
2025-11-27 22:30:49 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+BOT_NAME: "MAWRLD MD",
+ PREFIX: ".","/",
+OWNER_NAME: "Ridz Coder",
+OWNER_NUMBER: "263714732501",
+BOT_TOKEN: "8566420331:AAHxdrfy7SEK1Wm0ff6x4i-lShYjj0nPjVA",
+};
+export default config;
---
+++ index.js
@@ -0,0 +1,35 @@
+import config from './config.js';
+import { Telegraf } from 'telegraf';
+import chalk from 'chalk';
+import handler from './client.js';
+import { cmdLoader } from './lib/plugins.js';
+import Function from './lib/function.js';
+
+global.Func = Function;
+
+const bot = new Telegraf(process.config.BOT_TOKEN);
+
+(async () => {
+  try {
+    console.log(chalk.blue('Loading MAWRLD MD commands...'));
+    await cmdLoader(bot);
+
+    handler(bot);
+if (!bot) {
+
+  console.error(chalk.red.bold('Error: BOT_TOKEN not found in environment variables.'));
+
+  process.exit(1);
+
+}
+ 
+console.log(chalk.green.bold('MAWRLD-MD started successfully!'));
+    await bot.launch();
+
+  } catch (error) {
+    console.error(chalk.red.bold('Error starting bot:'), error);
+  }
+
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+})();
